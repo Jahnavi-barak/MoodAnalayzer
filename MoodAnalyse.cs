6e2d6bb4b122604330e3eedea32244359a9082a5
jahnavi <kosalajahnavi656@gmail.com>
2021-11-08 21:39:07 +0530

    HappyOrSad

---
+++ MoodAnalyse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer1
+{
+    public class MoodAnalyse
+    {
+        private string message;
+        public MoodAnalyse(String message)
+        {
+            this.message = message;
+        }
+        public string AnalayseMood(string message)
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
