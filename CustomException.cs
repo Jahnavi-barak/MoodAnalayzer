ec255c9b900e60391e3c48db7351d0d69e003bd4
jahnavi <kosalajahnavi656@gmail.com>
2021-11-11 13:37:32 +0530

    [CustomException UC1-3]

---
+++ CustomException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MoodAnalyzer1
+{
+    public class CustomException : Exception
+    {
+        /// <summary>
+        /// Enum for Exception Type.
+        /// </summary>
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, Empty_Message,
+            NO_SUCH_FIELD, NO_SUCH_METHOD,
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE,
+            EMPTY_MESSAGE
+        }
+        
+        /// Creating 'type' variable of type ExceptionType
+        private readonly ExceptionType type;
+
+        ///<summary>
+        ///parameterized Constructor sets the Exception Type and Message.
+        ///</summary>
+        ///<param name"Type></param>
+        ///<param name"message"></param>
+        public CustomException(ExceptionType Type, string message) : base(message)
+        { 
+           this.type = Type;
+        }
+    } 
+}
+
+         
